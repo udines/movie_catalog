6faf51edaeb6520989f43e6f133613c93788e83c
FATA <fataazharuddin.qontak@outlook.com>
2019-12-31 21:56:26 +0700

    Update repository

--- lib/services/movie_db_repository.dart
+++ lib/services/movie_db_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_movie_catalog/models/authentication/guest_session.dart';
 import 'package:flutter_movie_catalog/models/authentication/request_token.dart';
 import 'package:flutter_movie_catalog/models/authentication/session.dart';
+import 'package:flutter_movie_catalog/models/movies/movie_list.dart';
 import 'package:flutter_movie_catalog/services/movie_db_client.dart';
 
 class MovieDbRepository {
@@ -25,6 +26,26 @@ class MovieDbRepository {
 
   Future<Session> createSession(String validatedToken) async {
     final Session result = await client.createSession(validatedToken);
+    return result;
+  }
+
+  Future<MovieList> getNowPlayingMovies(int page) async {
+    final MovieList result = await client.getNowPlayingMovies(page);
+    return result;
+  }
+
+  Future<MovieList> getPopularMovies(int page) async {
+    final MovieList result = await client.getPopularMovies(page);
+    return result;
+  }
+
+  Future<MovieList> getTopRatedMovies(int page) async {
+    final MovieList result = await client.getTopRatedMovies(page);
+    return result;
+  }
+
+  Future<MovieList> getUpcomingMovies(int page) async {
+    final MovieList result = await client.getUpcomingMovies(page);
     return result;
   }
 }
