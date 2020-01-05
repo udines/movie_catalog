ded14c71f909e87fee45b59e58be6053cde5c88e
FATA <fataazharuddin.qontak@outlook.com>
2020-01-05 14:19:26 +0700

    Register bloc dependencies in main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_catalog/blocs/authentication/authentication_bloc.dart';
+import 'package:flutter_movie_catalog/blocs/movies/movie_bloc.dart';
+import 'package:flutter_movie_catalog/blocs/movies/movie_list_bloc.dart';
 import 'package:flutter_movie_catalog/screens/movies_screen.dart';
 import 'package:flutter_movie_catalog/services/movie_db_cache.dart';
 import 'package:flutter_movie_catalog/services/movie_db_client.dart';
@@ -7,15 +10,7 @@ import 'package:injector/injector.dart';
 
 void main() {
   final Injector injector = Injector.appInstance;
-
-  injector.registerSingleton<MovieDbClient>((_) => MovieDbClient());
-  injector.registerSingleton<MovieDbCache>((_) => MovieDbCache());
-  injector.registerSingleton<MovieDbRepository>((Injector injector) {
-    final MovieDbClient client = injector.getDependency<MovieDbClient>();
-    final MovieDbCache cache = injector.getDependency<MovieDbCache>();
-    return MovieDbRepository(client, cache);
-  });
-
+  _registerDependencies(injector);
   runApp(App());
 }
 
@@ -30,4 +25,26 @@ class App extends StatelessWidget {
       home: MoviesScreen()
     );
   }
+}
+
+void _registerDependencies(Injector injector) {
+  injector.registerSingleton<MovieDbClient>((_) => MovieDbClient());
+  injector.registerSingleton<MovieDbCache>((_) => MovieDbCache());
+  injector.registerSingleton<MovieDbRepository>((Injector injector) {
+    final MovieDbClient client = injector.getDependency<MovieDbClient>();
+    final MovieDbCache cache = injector.getDependency<MovieDbCache>();
+    return MovieDbRepository(client, cache);
+  });
+  injector.registerDependency<AuthenticationBloc>((Injector injector) {
+    final MovieDbRepository repository = injector.getDependency<MovieDbRepository>();
+    return AuthenticationBloc(repository);
+  });
+  injector.registerDependency<MovieListBloc>((Injector injector) {
+    final MovieDbRepository repository = injector.getDependency<MovieDbRepository>();
+    return MovieListBloc(repository);
+  });
+  injector.registerDependency<MovieBloc>((Injector injector) {
+    final MovieDbRepository repository = injector.getDependency<MovieDbRepository>();
+    return MovieBloc(repository);
+  });
 }
